60a502eac97d6c5e48201637de60e5816cea442d
gleb.erokhin <gleb.erokhin@gmail.com>
2025-02-10 06:59:37 +0300

    шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const clear = require('./task/clear.js');
 const html = require('./task/html.js');
 const js = require('./task/js.js');
 const img = require('./task/img.js');
+const font = require('./task/font.js');
 
 // сервер, перезагрузка страницы
 const server = () => {
@@ -24,16 +25,18 @@ const watcher = () => {
     watch(path.html.watch, html).on('all', bSync.reload);
     watch(path.js.watch, js).on('all', bSync.reload);
     watch(path.img.watch, img).on('all', bSync.reload);
+    watch(path.font.watch, font).on('all', bSync.reload);
 }
 
 exports.html = html;
 exports.js = js;
 exports.img = img;
+exports.font = font;
 exports.watch = watcher;
 exports.clear = clear;
 
 exports.default = series(
     clear,
-    parallel(html, js, img),
+    parallel(html, js, img, font),
     parallel(watcher, server)
 );
