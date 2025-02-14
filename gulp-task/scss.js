60fa4975d7fafd671911a92c064699bb36f69a15
gleb.erokhin <gleb.erokhin@gmail.com>
2025-02-14 22:45:26 +0300

    подключил корректировку путей

--- gulp-task/scss.js
+++ gulp-task/scss.js
@@ -1,27 +1,27 @@
-import gulp from'gulp';
+import gulp from 'gulp';
 
 //Конфигурация
-import path from'../gulp-config/path.js';
-import app from'../gulp-config/app.js';
+import path from '../gulp-config/path.js';
+import app from '../gulp-config/app.js';
 
 //Плагины
-import plumber from'gulp-plumber';
-import notify from'gulp-notify';
-import autoprefixer from'gulp-autoprefixer';
-import csso from'gulp-csso';
-import shorthand from'gulp-shorthand';
-import rename from'gulp-rename';
-import replace from'gulp-replace';
-import size from'gulp-size';
-import webpCss from'gulp-webp-css';
-import sourceMaps from'gulp-sourcemaps';
-import groupMediaQueries from'gulp-group-css-media-queries';
-import gulpIf from'gulp-if';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import autoprefixer from 'gulp-autoprefixer';
+import csso from 'gulp-csso';
+import shorthand from 'gulp-shorthand';
+import rename from 'gulp-rename';
+import replace from 'gulp-replace';
+import size from 'gulp-size';
+import webpCss from 'gulp-webp-css';
+import sourceMaps from 'gulp-sourcemaps';
+import groupMediaQueries from 'gulp-group-css-media-queries';
+import gulpIf from 'gulp-if';
 
-import dartSass from'sass';
-import gulpSass from'gulp-sass';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 const sass = gulpSass(dartSass);
-import sassGlob from'gulp-sass-glob';
+import sassGlob from 'gulp-sass-glob';
 
 
 //Обработка scss
@@ -46,27 +46,28 @@ const scss = () => {
         .pipe(gulpIf(app.isProd, groupMediaQueries()))
         .pipe(sass())
         .pipe(webpCss())
+        .pipe(
+            replace(
+                /(['"]?)(\.\.\/)+(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+                '$1$2$3$4$6$1'
+            )
+        )
         .pipe(sourceMaps.write())
-        .pipe(size({title: "main.css"}))
+        .pipe(size({ title: "main.css" }))
         .pipe(gulp.dest(path.scss.dest))
         .pipe(csso())
-        .pipe(rename({suffix: ".min"}))
-        .pipe(size({title: "main.min.css"}))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(size({ title: "main.min.css" }))
         .pipe(gulp.dest(path.scss.dest))
-        // .pipe(shorthand())
+    // .pipe(shorthand())
 
-        
-        // .pipe(
-        //     replace(
-        //         /(['"]?)(\.\.\/)+(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
-        //         '$1$2$3$4$6$1'
-        //     )
-        // )
-        // .pipe(size({title: "main.css"}))
-        // .pipe(gulp.dest(path.scss.dest))
-        // .pipe(rename({suffix: ".min"}))
-        // .pipe(csso())
-        // .pipe(gulp.dest(path.scss.dest, { sourcemaps: !app.isProd }))
+
+
+    // .pipe(size({title: "main.css"}))
+    // .pipe(gulp.dest(path.scss.dest))
+    // .pipe(rename({suffix: ".min"}))
+    // .pipe(csso())
+    // .pipe(gulp.dest(path.scss.dest, { sourcemaps: !app.isProd }))
 }
 
 export default scss;
