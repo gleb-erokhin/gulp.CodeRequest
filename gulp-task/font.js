29d55d47cee528dadad1d38050e2873d5cb6f2ee
gleb.erokhin <gleb.erokhin@gmail.com>
2025-03-01 21:09:38 +0300

    корректировка шрифтов, поправка настроек тасков

--- gulp-task/font.js
+++ gulp-task/font.js
@@ -21,8 +21,10 @@ const font = () => {
             }))
         }))
         .pipe(newer(path.font.dest))
-        .pipe(fonter(app.fonter))
-        .pipe(gulp.dest(path.font.dest))
+
+        // .pipe(fonter(app.fonter))
+        // .pipe(gulp.dest(path.font.dest))
+        
         // конвертация шрифтов в формат woff2
         .pipe(ttf2woff2())
         .pipe(gulp.dest(path.font.dest))
